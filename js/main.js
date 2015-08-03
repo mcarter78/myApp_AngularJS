10fdf0bcc44ff80e120c9902240ff71e642174ce
Matt Carter <sjsharksftw@gmail.com>
2015-08-03 18:37:35 +0000

    Added audio element to controller

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp', []);
 
 app.controller('PlayerController', ['$scope', function($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = '/media/gb.mp3';
 }]);
 
 app.controller('RelatedController', ['$scope', function($scope) {
