ac82b2f5c532a6cfecbe21feb93fe52ba2a6ed4e
Matt Carter <sjsharksftw@gmail.com>
2015-08-03 18:46:43 +0000

    Added play/stop functions to controller

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ app.controller('PlayerController', ['$scope', function($scope) {
   $scope.playing = false;
   $scope.audio = document.createElement('audio');
   $scope.audio.src = '/media/gb.mp3';
+  $scope.play = function() {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function() {
+    $scope.audio.stop();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop();
+    });
+  });
 }]);
 
 app.controller('RelatedController', ['$scope', function($scope) {
